9cf65ca9eddec8e39c4d4b841fbdccbcb02c38c6
haydendaly <44846368+haydendaly@users.noreply.github.com>
2019-06-23 17:08:28 -0400

    added tests

--- unittest/mainTest.py
+++ unittest/mainTest.py
@@ -1,5 +1,6 @@
 import unittest
 from userstories.jbUserStories import us01, us02
+from userstories.hdUserStories import us_35, us_36
 
 class Tests(unittest.TestCase):
     def test_us01(self):
@@ -12,5 +13,15 @@ class Tests(unittest.TestCase):
         input2 = {'@F1@': {'HUSB': '@I1@', 'WIFE': '@I2@', 'MARR': '6 MAR 2018'}}
         output = {'@F1@': {'HUSB': '@I1@', 'WIFE': '@I2@', 'MARR': '00 JAN 0000'}}
         self.assertEqual(us02(input, input2), output)
+        
+    def test_us35(self):
+        input = {'@I1@': {'NAME' : 'Hayden /Daly/', 'BIRT': '6 JUN 2019'}, '@I2@': {'NAME' : 'Todd /Daly/', 'BIRT': '7 MAR 2018'}}
+        output = ['@I1@', 'Hayden /Daly/', '6 JUN 2019']
+        self.assertEqual(us_35(input), output)
+
+    def test_us36(self):
+         input = {'@I1@': {'NAME' : 'Hayden /Daly/', 'DEAT': '6 JUN 2019'}, '@I2@': {'NAME' : 'Todd /Daly/', 'BIRT': '7 MAR 2018'}}
+         output = ['@I1@', 'Hayden /Daly/', '6 JUN 2019']
+         self.assertEqual(us_36(input), output)
 
 unittest.main()
