12332e5abfb24d3ea7cdf936dd8e8bd432d1103b
gilgga <gaustria@stevens.edu>
2019-06-28 22:58:42 -0400

    Restructure code to match program refactor, simplify code, and add extra condition to make sure that only dates within the 30 day recency range

--- userstories/hdUserStories.py
+++ userstories/hdUserStories.py
@@ -1,46 +1,33 @@
-import unittest
 import datetime
 import prettytable
 
-def us_35(individualData):
-    try:
-        recentBirtTable = prettytable.PrettyTable()
-        recentBirtTable.field_names = ['ID', 'Name', 'Birthday']
-    except:
-        return 'No module \'PrettyTable\''
+def us_35(GEDCOM_dict):
 
-    bool = False
-    rows = []
+    recentBirtTable = prettytable.PrettyTable()
+    recentBirtTable.field_names = ['ID', 'Name', 'Birthday']
 
-    for key, value in individualData.items():
-        if(datetime.datetime.strptime(value['BIRT'], '%d %b %Y') > datetime.datetime.now() + datetime.timedelta(-30)):
-            row = [key, value['NAME'], value['BIRT']]
-            rows += row
-            recentBirtTable.add_row(row)
-            bool = True
-    if bool == True:
-        print('Born Within the Past 30 Days')
-        print(recentBirtTable)
-        return rows
+    for key, value in GEDCOM_dict['individualData'].items():
+        if ( value['BIRT'] != 'N/A' ):
+            birthdate = datetime.datetime.strptime(" ".join( value['BIRT'].split('-') ), '%Y %m %d')
+            today = datetime.datetime.now()
 
-def us_36(individualData):
-    try:
-        recentDeatTable = prettytable.PrettyTable()
-        recentDeatTable.field_names = ["ID", "Name", "Death Date"]
-    except:
-        return 'No module \'PrettyTable\''
+            if( birthdate >= today + datetime.timedelta(-30) and birthdate <= today ):
+                row = [key, value['NAME'], value['BIRT']]
+                recentBirtTable.add_row(row)
 
-    bool = False
-    rows = []
+    return recentBirtTable
 
-    for key, value in individualData.items():
-        if 'DEAT' in value:
-            if(datetime.datetime.strptime(value['DEAT'], '%d %b %Y') > datetime.datetime.now() + datetime.timedelta(-30)):
+def us_36(GEDCOM_dict):
+
+    recentDeatTable = prettytable.PrettyTable()
+    recentDeatTable.field_names = ["ID", "Name", "Death"]
+
+    for key, value in GEDCOM_dict['individualData'].items():
+        if ( value['DEAT'] != 'N/A' ):
+            deathdate = datetime.datetime.strptime(" ".join(value['DEAT'].split('-') ), '%Y %m %d')
+            today = datetime.datetime.now()
+            if( deathdate >= today + datetime.timedelta(-30) and deathdate <= today ):
                 row = [key, value['NAME'], value['DEAT']]
-                rows += row
                 recentDeatTable.add_row(row)
-                bool = True
-    if bool == True:
-        print('Death Within the Past 30 Days')
-        print(recentDeatTable)
-        return rows
+
+    return recentDeatTable
