3483c652de4c03e47029ce3ab501d104e52c8f3e
gilgga <gaustria@stevens.edu>
2019-07-30 19:02:13 -0400

    Create Unit tests for us41 and us42

--- unittest/mainTest.py
+++ unittest/mainTest.py
@@ -8,7 +8,7 @@ from prettytable import PrettyTable;
 import unittest
 from jbUserStories import us01, us02, us03, us04, us05, us09
 from hdUserStories import us35, us36, us29, us39, us06, us07
-from gaUserStories import us38, us27, us30, us31, us10
+from gaUserStories import us38, us27, us30, us31, us10, us41, us42
 
 class Tests(unittest.TestCase):
     def test_us01(self):
@@ -337,4 +337,54 @@ class Tests(unittest.TestCase):
 
         output = testTable
         self.assertEqual( us09( input ).get_string(), output.get_string())
+
+
+    def test_us41(self):
+        input = "MAR 2000"  # Date without Days
+        output = "01 MAR 2000"
+
+        self.assertEqual( us41( input ), output )
+
+    def test_us41_2(self):
+        input = "1900"  # Date without Days and Months
+        output = "01 JAN 1900"
+
+        self.assertEqual( us41( input), output )
+
+    def test_us41_3(self):
+        input = "12 JUN 1900 09"    # Long Date
+        output = "N/A"
+
+        self.assertEqual( us41( input ), output )
+
+    def test_us41_4(self):
+        input = "18 SEP 1800"   # Complete Date
+        output = "18 SEP 1800"
+
+        self.assertEqual( us41( input ), output )
+
+    def test_us42(self):
+        input = "29 FEB 2015"   # Illegitimate Date - Leap Day on non-Leap Year
+        output = "N/A"
+
+        self.assertEqual( us42( input ), output )
+
+    def test_us42_2(self):
+        input = "42 MAR 2018"   # Illegitimate Date - Illegal Day
+        output = 'N/A'
+
+        self.assertEqual( us42( input ), output )
+
+    def test_us42_3(self):
+        input = "04 APR 1908"   # Legitimate Date
+        output = "04 APR 1908"
+
+        self.assertEqual( us42( input ), output )
+
+    def test_us42_4(self):
+        input = "07 NOV 3000"   # Date in the future
+        output = "07 NOV 3000"
+
+        self.assertEqual( us42( input ), output )
+
 unittest.main()
